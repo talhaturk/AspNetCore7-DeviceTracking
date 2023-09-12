4b2566ea4652eb95fde280e5e21b9d0bb9d218bb
Talha Türk <talha.turkk@outlook.com>
2023-09-12 13:39:07 +0300

    editorsite-devices-added

---
+++ Presentation/Areas/Editor/Controllers/DevicesController.cs
@@ -0,0 +1,32 @@
+﻿using Entities.Dtos;
+using Entities.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Services;
+using Services.Contracts;
+
+namespace Presentation.Areas.Editor.Controllers
+{
+    [Area("Editor")]
+    [Authorize(Roles = "Editor")]
+    public class DevicesController : Controller
+    {
+        private readonly IServiceManager _serviceManager;
+
+        public DevicesController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Device> devices = _serviceManager.DeviceService.GetAllDevices(false)
+                .OrderBy(d => d.DeviceId);
+            return View(devices);
+        }
+
+    }
+}
